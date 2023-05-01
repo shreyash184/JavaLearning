e8c9660072aa561ee082b4f8c826e3cd1c1c8345
Shreyash Chavhan <36036192+shreyash184@users.noreply.github.com>
2023-05-01 18:56:02 +0530

    What is the use of lombok is added

---
+++ JavaPractice/SpringBoot/Lombok/Lombok/src/main/java/com/shreyash/Lombok/Person.java
@@ -0,0 +1,15 @@
+package com.shreyash.Lombok;
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+@Getter
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
+public class Person {
+    private Integer id;
+    private String name;
+}
