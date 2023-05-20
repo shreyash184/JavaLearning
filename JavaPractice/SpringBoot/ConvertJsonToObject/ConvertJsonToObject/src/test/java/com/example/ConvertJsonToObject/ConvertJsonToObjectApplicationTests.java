e4959e4b77c0a6579806888ff84dbef9c367ac33
Shreyash Chavhan <36036192+shreyash184@users.noreply.github.com>
2023-05-20 21:51:21 +0530

    Json to Object

---
+++ JavaPractice/SpringBoot/ConvertJsonToObject/ConvertJsonToObject/src/test/java/com/example/ConvertJsonToObject/ConvertJsonToObjectApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.ConvertJsonToObject;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ConvertJsonToObjectApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
