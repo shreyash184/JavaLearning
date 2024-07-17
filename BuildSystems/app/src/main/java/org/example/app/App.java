c2149d122c88b4b39b28e7d0d1d510dbccd49754
Shreyash Chavhan <shreyashchavhan@Shreyashs-MacBook-Air.local>
2024-07-17 21:47:25 +0530

    Added configuration for gradle to work with cli

--- BuildSystems/app/src/main/java/org/example/app/App.java
+++ BuildSystems/app/src/main/java/org/example/app/App.java
@@ -3,8 +3,25 @@
  */
 package org.example.app;
 
+import okhttp3.OkHttp;
+import okhttp3.OkHttpClient;
+import okhttp3.Request;
+import okhttp3.Response;
+
+import java.io.IOException;
+
 public class App {
     public static void main(String[] args) {
-        System.out.println("Testing the gradle project");
+        OkHttpClient client = new OkHttpClient();
+
+        String url =  "https://fakestoreapi.com/products?limit=1";
+
+        Request request = new Request.Builder().url(url).build();
+
+        try(Response response = client.newCall(request).execute()){
+            System.out.println(response.body().string());
+        } catch (IOException e) {
+            throw new RuntimeException(e);
+        }
     }
 }
